93270a8366a9b2a26112562c474755190c353ed7
mao321 <mao321-ru@users.noreply.github.com>
2025-01-10 01:53:55 +0400

    [+] Использование логирования в сервлете
    Лог попадает в файл ${TOMCAT_BASE}/logs/catalina.YYYY-MM-DD.log

--- src/main/java/org/example/mbg/AppServlet.java
+++ src/main/java/org/example/mbg/AppServlet.java
@@ -6,11 +6,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import java.util.logging.Logger;
 
 public class AppServlet extends HttpServlet {
+
+    private final static Logger log = Logger.getLogger(org.example.mbg.AppServlet.class.getName());
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Date date = new Date();
+        log.info( "request received: date: " + date.toString());
         req.setAttribute("generatedTime", date.toString());
         req.getRequestDispatcher("views/index.jsp").forward(req, resp);
     }
