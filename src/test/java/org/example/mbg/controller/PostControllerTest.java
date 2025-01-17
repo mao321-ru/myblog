4b2038f741848e7cb4031ef660258de7cc0d4b00
mao321 <mao321-ru@users.noreply.github.com>
2025-01-18 01:50:24 +0400

    [+] Подключен JUnit 5 (junit-jupiter-api)

---
+++ src/test/java/org/example/mbg/controller/PostControllerTest.java
@@ -0,0 +1,14 @@
+package org.example.mbg.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PostControllerTest {
+
+    @Test
+    void findPostsTest() {
+        System.out.println( "findPostsTest: started");
+        assertEquals( 1, 1, "1 = 1 ?");
+    }
+}
