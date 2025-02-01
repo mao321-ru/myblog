adf093eda458f25d3d3588beb433e682e022b6a6
mao321 <mao321-ru@users.noreply.github.com>
2025-02-01 19:58:09 +0400

    [*] Уточнения по схеме (ограничения на пустые строки, макс. длина поста)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,9 @@
 -- Посты блога
 create table if not exists posts(
     post_id bigserial primary key,
-    title varchar(256) not null,
+    title varchar(256) not null check( trim( title) != ''),
     tags_str varchar(1000),
-    text varchar(4000),
+    text varchar(32000),
     likes_count integer check (likes_count >= 0),
     create_time timestamp with time zone default current_timestamp not null
 );
@@ -11,15 +11,15 @@ create table if not exists posts(
 -- Картинки постов
 create table if not exists post_images(
     post_id bigint not null unique references posts,
-    orig_filename varchar(256) not null,
-    content_type varchar(256) not null,
+    orig_filename varchar(256) not null check( trim( orig_filename) != ''),
+    content_type varchar(256) not null check( trim( content_type) != ''),
     file_data bytea not null
 );
 
 -- Теги (справочник)
 create table if not exists tags(
     tag_id bigserial primary key,
-    tag_name varchar(256) not null unique,
+    tag_name varchar(256) not null check( trim( tag_name) != '') unique,
     create_time timestamp with time zone default current_timestamp not null
 );
 
@@ -41,7 +41,7 @@ on
 create table if not exists post_comments(
     comment_id bigserial primary key,
     post_id bigint not null references posts,
-    comment_text varchar(4000) not null,
+    comment_text varchar(4000) not null check(trim( comment_text) != ''),
     create_time timestamp with time zone default current_timestamp not null
 );
 
